2f01acf6bb37c374924dd81df215447ec6259dd3
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-29 16:27:42 +0100

    Map: refactoring to be responsive, minor fixes

--- src/map.js
+++ src/map.js
@@ -1,119 +1,107 @@
 const d3 = require('d3');
 
-var svg_width = 800,
-    svg_height = 600;
-var markerGroup;
+var width = 600
+var height = 400
 
-var svg = d3.select('#map_container')
-            .append('svg')
-              .attr('width', svg_width)
-              .attr('height', svg_height)
-              .attr('fill', 'green')
-              .style('border', 15);
+var mapSvg = d3.select("#map_container")
+    .append("div")
+    // Container class to make it responsive.
+    .classed("svg-container", true)
+    .append("svg")
+    // Responsive SVG needs these 2 attributes and no width and height attr.
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + width + " " + height)
+    // Class to make it responsive.
+    .classed("svg-content-responsive", true);
 
-svg.append("rect")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("height", svg_height)
-    .attr("width", svg_width)
-    .style("stroke", 'black')
-    .style("fill", "none")
-    .style("stroke-width", 1);
+mapSvg.append("rect")
+    .classed("rect_b", true)
+    .attr("width", width)
+    .attr("height", height)
 
-var legend = svg.append('div')
-                  .attr('class', 'legend');
+// create a Geo Projection
+var projection = d3.geoMercator()
+    .translate([120, 600])
+    .scale(500)
+    .precision(10);
 
-legend.append('text')
-      .text('Test');
+var path = d3.geoPath()
+    .projection(projection);
 
 //Setting up brush's area
 var brush = d3.brush()
-              .extent([[0, 0], [svg_width, svg_height]])
-                .on('start brush end', brushed);
-
-
-var projection = d3.geoMercator()
-                   .translate([svg_width/5, svg_height + svg_height /2])
-                   .scale(750)
-                   .precision(10);
-
-var path = d3.geoPath()
-             .projection(projection);
+    .extent([
+        [0, 0],
+        [width, height]
+    ]).on('start brush end', brushed);
 
 function populateMap(map) {
-  //console.log(map.features);
+    //console.log(map.features);
 
-  svg.selectAll('path')
-     .data(map.features)
-     .enter()
-     .append('path')
-      .attr('class', 'state')
-      .attr('id', function(d) {
-        return d.properties['OBJECTID'];
-      })
-      .attr('d', path);
+    mapSvg.selectAll('path')
+        .data(map.features)
+        .enter()
+        .append('path')
+        .attr('class', 'state')
+        .attr('id', function(d) {
+            return d.properties['OBJECTID'];
+        })
+        .attr('d', path);
 }
 
+var markerGroup;
 
 function addMarkers(battles) {
-  markerGroup = svg.append('g')
-                    .attr('class', 'brush')
-                   .call(brush);
+    markerGroup = mapSvg.append('g')
+        .attr('class', 'brush')
+        .call(brush);
 
+    markerGroup.append('g')
+        .selectAll('circle')
+        .data(battles)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) {
+            return projection([d.longitude, d.latitude])[0];
+        })
+        .attr('cy', function(d) {
+            return projection([d.longitude, d.latitude])[1];
+        })
+        .attr('r', 4)
+        .attr('fill', 'blue')
+        .attr('pointer-events', 'all')
+        .on('click', function(d) {
+            d3.selectAll('.selected')
+                .classed('selected', false);
 
-  svg.select('g.brush')
-     .select('rect.overlay')
-     .style('visibility', 'hidden');
+            d3.select(this)
+                .classed('selected', true);
 
-  markerGroup.append('g')
-     .selectAll('circle')
-             .data(battles)
-             .enter()
-             .append('circle')
-              .attr('cx', function(d) {
-                  return projection([d.longitude, d.latitude])[0];
-              })
-              .attr('cy', function(d) {
-                return projection([d.longitude, d.latitude])[1];
-              })
-             .attr('r', 4)
-             .attr('fill', 'blue')
-             .attr('pointer-events', 'all')
-             .on('click', function(d) {
-               d3.selectAll('.selected')
-                 .classed('selected', false);
+            console.log(d.label);
+        })
 
-               d3.select(this)
-                 .classed('selected', true);
-
-               console.log(d.label);
-             })
-
-             .style('visibility', 'hidden');
+    .style('visibility', 'visible');
 }
 
 function brushed() {
-  var selection = d3.event.selection;
+    var selection = d3.event.selection;
 
-  if (selection) {
-    markerGroup.selectAll('circle')
-               .style('visibility', function(d) {
-                 var cx = d3.select(this).attr('cx');
-                 var cy = d3.select(this).attr('cy');
-                 //Check if the point is inside the brushed area
-                 var isBrushed = (cx >= selection[0][0] && cx <= selection[1][0] &&
+    if (selection) {
+        markerGroup.selectAll('circle')
+            .style('visibility', function(_d) {
+                var cx = d3.select(this).attr('cx');
+                var cy = d3.select(this).attr('cy');
+                //Check if the point is inside the brushed area
+                var isBrushed = (cx >= selection[0][0] && cx <= selection[1][0] &&
                     cy >= selection[0][1] && cy <= selection[1][1]);
 
-                if(isBrushed)
-                  return 'visible';
-                else
-                  return 'hidden';
-               });
-  }
+                return isBrushed ? 'visible' : 'hidden';
+            });
+    } else markerGroup.selectAll('circle')
+        .style('visibility', "visible");
 }
 
-
 export default {
-  populateMap : populateMap,
-  addMarkers : addMarkers
+    populateMap: populateMap,
+    addMarkers: addMarkers
 }
