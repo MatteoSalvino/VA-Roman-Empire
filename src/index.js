9efc45ca431c65c137f963bde425b3dade15e0ca
Luigi <russo.1699981@studenti.uniroma1.it>
2020-03-01 13:09:12 +0100

    add and parse wars dataset

--- src/index.js
+++ src/index.js
@@ -3,24 +3,27 @@ import './index.scss'
 import mapBuilder from './map'
 import lineChartBuilder from './lineChart'
 import barChartBuilder from './barchart'
+import boxplotBuilder from './boxPlot'
 
 const d3 = require('d3');
 
 Promise.all([
     d3.json('./empire_map.json'),
-    d3.csv('/battles.csv')
+    d3.csv('/battles.csv'),
+    d3.csv('/wars.csv')
 ]).then(function(data) {
     var map = data[0],
-        battles = data[1];
-
+        battles = data[1],
+        wars = data[2];
 
     mapBuilder.populateMap(map);
     mapBuilder.addMarkers(battles);
 
     lineChartBuilder.populateChart(battles);
-
     barChartBuilder.populateChart(battles);
+    boxplotBuilder.populate(wars);
 
 }).catch(function(error) {
+    console.log(error);
     throw error;
 });
