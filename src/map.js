a1b110c3b9b8f78abbfada83e8ce22629f5ea249
Luigi <russo.1699981@studenti.uniroma1.it>
2020-03-01 09:11:41 +0100

    show a recap when multiple multiples items are brushed on map

--- src/map.js
+++ src/map.js
@@ -28,37 +28,7 @@ mapSvg.append("rect")
     .attr("width", width)
     .attr("height", height)
 
-var legend = mapSvg.append("svg")
-                    .attr("width", 200)
-                    .attr("height", 100)
-                    .attr('x', 400)
-                    .attr('y', 10);
-
-legend.append('text')
-        .attr('id', 'battle_label')
-        .attr('x', 5)
-        .attr('y', 20)
-        .attr('font-size', 12)
-        .attr('font-weight', 'bold');
-
-legend.append('text')
-          .attr('id', 'battle_year')
-          .attr('x', 5)
-          .attr('y', 40)
-          .attr('font-size', 10);
-
-legend.append('text')
-        .attr('id', 'battle_coordinate')
-        .attr('x', 5)
-        .attr('y', 60)
-        .attr('font-size', 10);
-
-legend.append('text')
-      .attr('id', 'battle_outcome')
-      .attr('x', 5)
-      .attr('y', 80)
-      .attr('font-size', 10);
-
+var legend = setupLegend();
 
 // create a Geo Projection
 var projection = d3.geoMercator()
@@ -91,7 +61,6 @@ function populateMap(map) {
 }
 
 var markerGroup;
-
 
 function addMarkers(battles) {
     markerGroup = mapSvg.append('g')
@@ -128,6 +97,10 @@ function addMarkers(battles) {
 function brushed() {
     var selection = d3.event.selection;
 
+    var points = 0;
+    var minYear = Infinity;
+    var maxYear = -Infinity;
+
     if (selection) {
         markerGroup.selectAll('circle')
             .style('visibility', function(d) {
@@ -137,39 +110,45 @@ function brushed() {
                 var isBrushed = (cx >= selection[0][0] && cx <= selection[1][0] &&
                     cy >= selection[0][1] && cy <= selection[1][1]);
 
-                if(isBrushed) {
-                  d3.select(this)
-                    .attr('stroke-width', 0.5)
-                    .attr('stroke', 'white');
+                if (isBrushed) {
+                    points++;
+                    minYear = d3.min([minYear, +d.year]);
+                    maxYear = d3.max([maxYear, +d.year]);
+                    d3.select(this)
+                        .attr('stroke-width', 0.5)
+                        .attr('stroke', 'white');
 
                     legend.select('#battle_label')
-                          .text(d.label);
+                        .text(d.label);
                     legend.select('#battle_year')
-                          .text('Year : ' + d.year);
+                        .text('Year : ' + d.year);
                     legend.select('#battle_coordinate')
-                          .text('Coordinate : (' + d.latitude + ',' + d.longitude + ')');
+                        .text('Coordinate : (' + d.latitude + ',' + d.longitude + ')');
                     legend.select('#battle_outcome')
-                          .text('Outcome : ' + d.outcome);
-                  return 'visible';
-                } else{
-                  d3.select(this)
-                    .attr('stroke-width', 0);
-                  return 'hidden';
+                        .text('Outcome : ' + d.outcome);
+                    return 'visible';
                 }
+                d3.select(this).attr('stroke-width', 0);
+                return 'hidden';
             });
     } else {
-      markerGroup.selectAll('circle')
-        .style('visibility', "visible");
+        markerGroup.selectAll('circle')
+            .style('visibility', "visible");
+        resetLegend();
+    }
+    if (points > 1) {
+        resetLegend();
+        legend.select('#battle_label')
+            .text(points + " battles selected");
+        legend.select('#battle_year')
+            .text('From ' + parseRoman(minYear) + ' to ' + parseRoman(maxYear));
+    }
+}
 
-      legend.select('#battle_label')
-            .text('');
-      legend.select('#battle_year')
-            .text('');
-      legend.select('#battle_coordinate')
-            .text('');
-      legend.select('#battle_outcome')
-            .text('');
-      }
+function parseRoman(y) {
+    if (y == 0) return 0;
+    if (y < 0) return -y + "BC";
+    return y + "AD"
 }
 
 /*
@@ -181,6 +160,51 @@ function zoomed() {
               .attr('transform', d3.event.transform);
 }
 */
+
+function setupLegend() {
+    var legend = mapSvg.append("svg")
+        .attr("width", 200)
+        .attr("height", 100)
+        .attr('x', 400)
+        .attr('y', 10);
+
+    legend.append('text')
+        .attr('id', 'battle_label')
+        .attr('x', 5)
+        .attr('y', 20)
+        .attr('font-size', 12)
+        .attr('font-weight', 'bold');
+
+    legend.append('text')
+        .attr('id', 'battle_year')
+        .attr('x', 5)
+        .attr('y', 40)
+        .attr('font-size', 10);
+
+    legend.append('text')
+        .attr('id', 'battle_coordinate')
+        .attr('x', 5)
+        .attr('y', 60)
+        .attr('font-size', 10);
+
+    legend.append('text')
+        .attr('id', 'battle_outcome')
+        .attr('x', 5)
+        .attr('y', 80)
+        .attr('font-size', 10);
+    return legend;
+}
+
+function resetLegend() {
+    legend.select('#battle_label')
+        .text('');
+    legend.select('#battle_year')
+        .text('');
+    legend.select('#battle_coordinate')
+        .text('');
+    legend.select('#battle_outcome')
+        .text('');
+}
 
 export default {
     populateMap: populateMap,
